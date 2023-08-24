9b6873e512901d4b7d9c8c13a2dfd809a4ab0fdc
stephanie <stephanie.fadel@hotmail.com>
2023-08-24 19:21:17 +0200

    camera fixed

--- screens/profileScreen.js
+++ screens/profileScreen.js
@@ -1,16 +1,20 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import { historyHunts } from './HistoryHunterContent'; // Importera innehållet
+import { useNavigation } from '@react-navigation/native';
+
 
 export function ProfileScreen() {
+  const navigation = useNavigation();
+  
   const [profileImage, setProfileImage] = useState(null);
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraRef, setCameraRef] = useState(null);
   const [isCameraOpen, setIsCameraOpen] = useState(false);
 
   const handleCameraLaunch = async () => {
-    const { status } = await Camera.requestPermissionsAsync();
+    const { status } = await Camera.requestCameraPermissionsAsync();
     if (status === 'granted') {
       setHasPermission(true);
       setIsCameraOpen(true);
@@ -52,17 +56,15 @@ export function ProfileScreen() {
             type={Camera.Constants.Type.back}
           >
             <View style={styles.captureContainer}>
-             
+              <TouchableOpacity style={styles.captureButton} onPress={handleCapture}>
+                <Text style={styles.captureButtonText}>Capture</Text>
+              </TouchableOpacity>
             </View>
-            <TouchableOpacity style={styles.captureButton} onPress={handleCapture}>
-           <Text style={styles.captureButtonText}>Capture</Text>
-         </TouchableOpacity>
           </Camera>
-           
         )}
 
         <Text style={styles.username}>John Doe</Text>
-        <TouchableOpacity style={styles.createHuntButton}>
+        <TouchableOpacity style={styles.createHuntButton} onPress={() => navigation.navigate('createHunt')}>
           <Text style={styles.createHuntButtonText}>Create Hunt</Text>
         </TouchableOpacity>
         {historyHunts.map((hunt, index) => (
@@ -83,120 +85,116 @@ export function ProfileScreen() {
   );
 }
 
-
-        const styles = StyleSheet.create({
-          container: {
-            flex: 1,
-            backgroundColor: '#F3EFE7',
-          },
-          profileInfo: {
-            flex: 1,
-            alignItems: 'center',
-            marginTop: 20,
-          },
-          profileImageContainer: {
-            width: 120,
-            height: 120,
-            borderRadius: 60,
-            overflow: 'hidden',
-          },
-          camera: {
-            width: 300,
-            height: 300,
-          },
-          captureContainer: {
-            flex: 1,
-            alignItems: 'center',
-            marginBottom: 20,
-
-          },
-          captureButton: {
-            backgroundColor: '#456268',
-            padding: 20,
-            alignItems: 'center',
-
-          },
-          captureButtonText: {
-            color: 'white',
-            fontWeight: 'bold',
-            fontSize: 18,
-
-          },
-          defaultProfileImage: {
-            width: 120,
-            height: 120,
-            borderRadius: 60,
-            backgroundColor: 'white',
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-          defaultProfileText: {
-            fontSize: 16,
-            color: '#456268',
-          },
-          profileImage: {
-            width: 120,
-            height: 120,
-            borderRadius: 60,
-            marginBottom: 10,
-          },
-          username: {
-            fontSize: 24,
-            fontWeight: 'bold',
-            color: '#456268',
-            marginTop: 15,
-          },
-          createHuntButton: {
-            marginTop: 10,
-            backgroundColor: '#456268',
-            paddingVertical: 10,
-            paddingHorizontal: 20,
-            borderRadius: 5,
-            marginBottom: 40,
-          },
-          createHuntButtonText: {
-            color: 'white',
-            fontSize: 16,
-          },
-          section: {
-            marginVertical: 15,
-            paddingHorizontal: 20,
-          },
-          sectionTitle: {
-            fontSize: 18,
-            fontWeight: 'bold',
-            color: '#456268',
-            width: 300,
-          },
-          sectionDescription: {
-            fontSize: 14,
-            width: 300,
-            color: '#456268',
-          },
-          descriptionContainer: {
-            flexDirection: 'row',
-            alignItems: 'center',
-          },
-          descriptionImage: {
-            width: 30,
-            height: 30,
-            borderRadius: 50,
-            marginRight: 10,
-          },
-          sectionCenter: {
-            alignItems: 'center',
-            marginVertical: 20,
-          },
-          centeredSectionTitle: {
-            fontSize: 24,
-            fontWeight: 'bold',
-            color: '#456268',
-          },
-          line: {
-            width: 50,
-            height: 2,
-            backgroundColor: '#456268',
-            marginVertical: 5,
-          },
-        });
-        
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3EFE7',
+  },
+  profileInfo: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  profileImageContainer: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    overflow: 'hidden',
+  },
+  camera: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+  },
+  captureContainer: {
+    flex: 1,
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  captureButton: {
+    backgroundColor: '#456268',
+    padding: 20,
+    alignItems: 'center',
+  },
+  captureButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  defaultProfileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  defaultProfileText: {
+    fontSize: 16,
+    color: '#456268',
+  },
+  profileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+    marginBottom: 10,
+  },
+  username: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#456268',
+    marginTop: 15,
+  },
+  createHuntButton: {
+    marginTop: 10,
+    backgroundColor: '#456268',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginBottom: 40,
+  },
+  createHuntButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  section: {
+    marginVertical: 15,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#456268',
+    width: 300,
+  },
+  sectionDescription: {
+    fontSize: 14,
+    width: 300,
+    color: '#456268',
+  },
+  descriptionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  descriptionImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
+    marginRight: 10,
+  },
+  sectionCenter: {
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  centeredSectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#456268',
+  },
+  line: {
+    width: 50,
+    height: 2,
+    backgroundColor: '#456268',
+    marginVertical: 5,
+  },
+});
