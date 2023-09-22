45fce31fa55b8fccaafe863563e8ed53ad6dd2a7
stephanie <stephanie.fadel@hotmail.com>
2023-09-22 15:34:57 +0200

    changed: title

--- screens/HistoryHunterContent.js
+++ screens/HistoryHunterContent.js
@@ -1,11 +1,11 @@
 export const historyHunts = [
     {
-      title: "Planned Hunts",
+      title: "Active Hunts",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
       image: require('../assets/cookies.jpg'), // Ange sökvägen till bilden
     },
     {
-      title: "La Hunts",
+      title: "Planned Hunts",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       image: require('../assets/cookies.jpg'), // Ange sökvägen till bilden
     },
