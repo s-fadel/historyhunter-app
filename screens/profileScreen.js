e7e420d32526c1e1bef2a2ec743cdaf746ca633f
stephanie <stephanie.fadel@hotmail.com>
2023-09-24 15:39:23 +0200

    add: hunt to profilepage

--- screens/profileScreen.js
+++ screens/profileScreen.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera';
-import { historyHunts } from './HistoryHunterContent'; // Importera innehållet
-import { useNavigation } from '@react-navigation/native';
-import { AuthContext } from '../storage/AuthContext';
-import * as http from '../util/http';
-import { EvilIcons } from '@expo/vector-icons';
+import React, { useState, useEffect, useContext } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from "../storage/AuthContext";
+import * as http from "../util/http";
+import { EvilIcons } from "@expo/vector-icons";
+import { getHunt } from "../util/http";
+import { historyHunts } from "./HistoryHunterContent";
 
 export function ProfileScreen() {
   const navigation = useNavigation();
-  
   const [profileImage, setProfileImage] = useState(null);
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraRef, setCameraRef] = useState(null);
   const [isCameraOpen, setIsCameraOpen] = useState(false);
-
   const authCtx = useContext(AuthContext);
+  const [plannedHunts, setPlannedHunts] = useState([]);
+
+  console.log(plannedHunts, "plannedHunts");
 
   const handleCameraLaunch = async () => {
     const { status } = await Camera.requestCameraPermissionsAsync();
-    if (status === 'granted') {
+    if (status === "granted") {
       setHasPermission(true);
       setIsCameraOpen(true);
     } else {
@@ -36,74 +38,119 @@ export function ProfileScreen() {
     }
   };
 
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const userData = await http.getUserById(authCtx.token); // Fetch user from Firebase
-        const foundUser = userData.find(u => u.email.toLowerCase() === authCtx.email.toLowerCase()); // Modify according to your data structure
+        const foundUser = userData.find(
+          (u) => u.email.toLowerCase() === authCtx.email.toLowerCase()
+        ); // Modify according to your data structure
 
         if (foundUser) {
           setUsername(foundUser.displayName);
         }
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
 
     fetchUserData();
   }, []);
 
+  useEffect(() => {
+    const fetchHunt = async (userId) => {
+      try {
+        const hunt = await getHunt(userId);
+        if (hunt) {
+          setPlannedHunts(hunt);
+        }
+        return hunt;
+      } catch (error) {
+        console.error("Error fetching hunt details:", error);
+      }
+    };
+    fetchHunt(authCtx.email);
+  }, []);
+  console.log(plannedHunts, "PLANNEDHUNTS");
+
   return (
-  
     <View style={styles.container}>
-<View style={styles.profileInfo}>
-  {!isCameraOpen && (
-    <View style={styles.profileImageContainer}>
-      <TouchableOpacity onPress={handleCameraLaunch}>
-        {profileImage ? (
-          <Image source={{ uri: profileImage }} style={styles.profileImage} />
-        ) : (
-          <View style={styles.defaultProfileImage}>
-            <Text style={styles.defaultProfileText}>Add Photo</Text>
+      <View style={styles.profileInfo}>
+        {!isCameraOpen && (
+          <View style={styles.profileImageContainer}>
+            <TouchableOpacity onPress={handleCameraLaunch}>
+              {profileImage ? (
+                <Image
+                  source={{ uri: profileImage }}
+                  style={styles.profileImage}
+                />
+              ) : (
+                <View style={styles.defaultProfileImage}>
+                  <Text style={styles.defaultProfileText}>Add Photo</Text>
+                </View>
+              )}
+            </TouchableOpacity>
           </View>
         )}
-      </TouchableOpacity>
-    </View>
-  )}
 
-  {isCameraOpen && (
-    <Camera
-      style={styles.camera}
-      ref={(ref) => setCameraRef(ref)}
-      type={Camera.Constants.Type.back}
-    >
-      <View style={styles.cameraContent}>
-        <View style={styles.cameraCaptureContainer}>
-          <TouchableOpacity style={styles.captureButton} onPress={handleCapture}>
-          <EvilIcons name="camera" size={40} color="white"/>
-          </TouchableOpacity>
-        </View>
-      </View>
-    </Camera>
-  )}
+        {isCameraOpen && (
+          <Camera
+            style={styles.camera}
+            ref={(ref) => setCameraRef(ref)}
+            type={Camera.Constants.Type.back}
+          >
+            <View style={styles.cameraContent}>
+              <View style={styles.cameraCaptureContainer}>
+                <TouchableOpacity
+                  style={styles.captureButton}
+                  onPress={handleCapture}
+                >
+                  <EvilIcons name="camera" size={40} color="white" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Camera>
+        )}
 
+        <Text style={styles.username}>{username}</Text>
 
-<Text style={styles.username}>{username}</Text>
-
-        <TouchableOpacity style={styles.createHuntButton} onPress={() => navigation.navigate('createHunt')}>
+        <TouchableOpacity
+          style={styles.createHuntButton}
+          onPress={() => navigation.navigate("createHunt")}
+        >
           <Text style={styles.createHuntButtonText}>Create Hunt</Text>
         </TouchableOpacity>
-        {historyHunts.map((hunt, index) => (
+
+        <Text style={styles.sectionTitle}>{historyHunts[0].title}</Text>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.sectionDescription}>
+            {historyHunts[0].description}
+          </Text>
+        </View>
+        {plannedHunts.map((hunt, index) => (
           <View style={styles.section} key={index}>
-            <Text style={styles.sectionTitle}>{hunt.title}</Text>
-            <View style={styles.descriptionContainer}>
-              <Image source={hunt.image} style={styles.descriptionImage} />
-              <Text style={styles.sectionDescription}>{hunt.description}</Text>
-            </View>
+            <TouchableOpacity
+              style={styles.huntCard}
+              onPress={() => navigation.navigate("overviewMap", { hunt: hunt })} // Välj jakten när den trycks
+            >
+              <Text
+                style={[
+                  styles.sectionTitleRoute,
+                  {
+                    color: "#ffa953",
+                    textDecorationLine: "underline",
+                    fontWeight: "regular",
+                  },
+                ]}
+              >
+                {hunt.name}
+              </Text>
+            </TouchableOpacity>
           </View>
         ))}
+
         <View style={styles.sectionCenter}>
           <Text style={styles.centeredSectionTitle}>Medals</Text>
           <View style={styles.line} />
@@ -116,52 +163,52 @@ export function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F3EFE7',
+    backgroundColor: "#F3EFE7",
   },
   profileInfo: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 20,
   },
   profileImageContainer: {
     width: 120,
     height: 120,
     borderRadius: 60,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   camera: {
-    width: 200 ,
+    width: 200,
     height: 200,
     borderRadius: 100,
   },
   captureContainer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 20,
   },
   captureButton: {
-    backgroundColor: '#456268',
-    alignItems: 'center',
+    backgroundColor: "#456268",
+    alignItems: "center",
     width: 70,
     height: 70,
     borderRadius: 50,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   captureButtonText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
   },
   defaultProfileImage: {
     width: 120,
     height: 120,
     borderRadius: 60,
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
   },
   defaultProfileText: {
     fontSize: 16,
-    color: '#456268',
+    color: "#456268",
   },
   profileImage: {
     width: 120,
@@ -171,40 +218,45 @@ const styles = StyleSheet.create({
   },
   username: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#456268',
+    fontWeight: "bold",
+    color: "#456268",
     marginTop: 15,
   },
   createHuntButton: {
     marginTop: 10,
-    backgroundColor: '#456268',
+    backgroundColor: "#456268",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
     marginBottom: 40,
   },
   createHuntButtonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
   section: {
-    marginVertical: 15,
-    paddingHorizontal: 20,
+    marginVertical: 5,
   },
   sectionTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color: '#456268',
+    fontWeight: "bold",
+    color: "#456268",
     width: 300,
   },
   sectionDescription: {
     fontSize: 14,
     width: 300,
-    color: '#456268',
+    color: "#456268",
+    marginBottom: 10,
+  },
+  sectionTitleRoute: {
+    fontSize: 14,
+    color: "#456268",
+    width: 300,
   },
   descriptionContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   descriptionImage: {
     width: 30,
@@ -213,18 +265,18 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   sectionCenter: {
-    alignItems: 'center',
+    alignItems: "center",
     marginVertical: 20,
   },
   centeredSectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold',
-    color: '#456268',
+    fontWeight: "bold",
+    color: "#456268",
   },
   line: {
     width: 50,
     height: 2,
-    backgroundColor: '#456268',
+    backgroundColor: "#456268",
     marginVertical: 5,
   },
 });
